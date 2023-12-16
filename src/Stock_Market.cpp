d73f1bca9ee6a10bdcf8472a583ef0be3c6db046
j0nl925 <70623244+j0nl925@users.noreply.github.com>
2023-12-16 13:30:51 +0000

    added stock market files

---
+++ src/Stock_Market.cpp
@@ -0,0 +1,15 @@
+#include "Stock_Market.hpp"
+
+// Constructor to initialise variables
+Stock_Market::Stock_Market(){
+    double initial_price = 100; // Initial price
+    double mu = 0;  // Drift
+    double sigma = 0.5; // Volatility
+    int num_of_days = 128; // Number of days to predict for
+}
+
+// Function to simulate geometric brownian motion
+void Stock_Market::Simulate_GBM(){
+    
+
+}
---
+++ src/Stock_Market.hpp
@@ -0,0 +1,13 @@
+#ifndef STOCK_MARKET_HPP
+#define STOCK_MARKET_HPP
+
+class Stock_Market{
+    public:
+    // Constructor
+    Stock_Market();
+
+    // Functions
+    void Simulate_GBM();
+};
+
+#endif
