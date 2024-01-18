b88073faa5429767492764cb9a814703bb6f10dc
Salvador <s.navarro@imperial.ac.uk>
2024-01-18 10:42:15 +0000

    SDE fixes

--- tst/main.cpp
+++ tst/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Stock_Market.hpp"
 
 using namespace std;
@@ -7,9 +8,14 @@ int main() {
     Stock_Market stock_instance;
     auto stock_prices = stock_instance.Simulate_GBM();
 
+    ofstream MyFile("days.csv");
     for (int i = 0; i < stock_prices.first.size(); ++i) {
         std::cout << "Day " << i << ": Opening Price = " << stock_prices.first[i] << ", Closing Price = " << stock_prices.second[i] << std::endl;
+   
+        MyFile << i << " " <<  stock_prices.first[i] << std::endl;
+        
     }
+    MyFile.close();
 
     return 0;
 }
